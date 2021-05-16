12765db99fa110a0eba6db5d6aebcb6a886f4f04
abhi314 <patil.abhishek314@gmail.com>
2021-05-16 15:54:55 +0530

    minor change

--- src/main/java/com/abhi/cowinalarm/CowinalarmApplication.java
+++ src/main/java/com/abhi/cowinalarm/CowinalarmApplication.java
@@ -19,76 +19,80 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Arrays;
 import java.util.List;
-
+/**
+ * @author Abhishek Patil
+ * A simple scheduler to check
+ * vaccine availability on cowin.gov.in
+ */
 @SpringBootApplication
 @EnableScheduling
 public class CowinalarmApplication {
-    private static final Logger log = LoggerFactory.getLogger(CowinalarmApplication.class);
-    private final String[] pincode = {"400703", "400709"};
-    private final String[] date = {"17-05-2021"};
+	private static final Logger log = LoggerFactory.getLogger(CowinalarmApplication.class);
+	private final String[] pincode = {"400701", "400703", "400705", "400708", "400709"};
+	private final String[] date = {"16-05-2021","17-05-2021"};
 
-    public static void main(String[] args) {
-        SpringApplication.run(CowinalarmApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CowinalarmApplication.class, args);
+	}
 
-    @Scheduled(fixedDelay = 5000)
-    public void scheduleFixedDelayTask() {
-        Arrays.stream(pincode).forEach(p -> {
-                    Arrays.stream(date).forEach(d -> {
-                                HttpResponse<String> resp = null;
-                                try {
-                                    resp = getResp(p, d);
-                                } catch (UnirestException e) {
-                                    e.printStackTrace();
-                                }
-                                assert resp != null;
-                                if (resp.getStatus() != 200) {
-                                    log.info("Pincode: {}, Date: {}, Error while calling the API: {}",
-                                            p, d, resp.getStatusText());
-                                    return;
-                                }
-                                RootResp data = getRootResp(resp);
-                                if (data.sessions.size() > 0) {
-                                    dataCheck(data.sessions, p, d);
-                                }
-                            }
-                    );
-                }
-        );
-    }
+	@Scheduled(fixedDelay = 5000)
+	public void scheduleFixedDelayTask() {
+		Arrays.stream(pincode).forEach(p -> {
+					Arrays.stream(date).forEach(d -> {
+								HttpResponse<String> resp = null;
+								try {
+									resp = getResp(p, d);
+								} catch (UnirestException e) {
+									e.printStackTrace();
+								}
+								assert resp != null;
+								if (resp.getStatus() != 200) {
+									log.info("Pincode: {}, Date: {}, Error while calling the API: {}",
+											p, d, resp.getStatusText());
+									return;
+								}
+								RootResp data = getRootResp(resp);
+								if (data.sessions.size() > 0) {
+									dataCheck(data.sessions, p, d);
+								}
+							}
+					);
+				}
+		);
+	}
 
-    private RootResp getRootResp(HttpResponse<String> resp) {
-        return new Gson().fromJson(resp.getBody(), RootResp.class);
-    }
+	private RootResp getRootResp(HttpResponse<String> resp) {
+		return new Gson().fromJson(resp.getBody(), RootResp.class);
+	}
 
-    private HttpResponse<String> getResp(String pincode, String date) throws UnirestException {
-        Unirest.setTimeouts(0, 0);
-        return Unirest.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?" +
-                "pincode=" + pincode + "&date=" + date)
-                .header("accept", "application/json")
-                .header("Accept-Language", "hi_IN")
-                .header("User-Agent", "PostmanRuntime/7.26.10")
-                .asString();
-    }
+	private HttpResponse<String> getResp(String pincode, String date) throws UnirestException {
+		Unirest.setTimeouts(0, 0);
+		return Unirest.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?" +
+				"pincode=" + pincode + "&date=" + date)
+				.header("accept", "application/json")
+				.header("Accept-Language", "hi_IN")
+				.header("User-Agent", "PostmanRuntime/7.26.10")
+				.asString();
+	}
 
-    private void dataCheck(List<Session> sessionList, String pincode, String date) {
-        if (sessionList.size() > 0) {
-            Session temp = sessionList.get(0);
-            log.info("\nPincode: {}, Date: {}, Min-Age:{}+, capacity: {}, fee: {}, vaccine: {}",
-                    pincode, date, temp.getMin_age_limit(), temp.getAvailable_capacity(), temp.getFee(), temp.getVaccine());
-            play();
-        }
-    }
+	private void dataCheck(List<Session> sessionList, String pincode, String date) {
+		if (sessionList.size() > 0) {
+			Session temp = sessionList.get(0);
+			log.info("\nPincode: {}, Date: {}, Min-Age:{}+, capacity: {}, fee: {}, vaccine: {}",
+					pincode, date, temp.getMin_age_limit(), temp.getAvailable_capacity(), temp.getFee(), temp.getVaccine());
+			play();
+		}
+	}
 
-    private void play() {
-        try {
-            JFXPanel j = new JFXPanel();
-            String uriString = new ClassPathResource("nuke_alarm.mp3").getFile().toURI().toString();
-            MediaPlayer Player = new MediaPlayer(new Media(uriString));
-            Player.play();
-        } catch (Exception ex) {
-            log.error("Error while playing media" + ex.getMessage(), ex);
-        }
-    }
+	private void play() {
+		try {
+			JFXPanel j = new JFXPanel();
+			String uriString = new ClassPathResource("nuke_alarm.mp3").getFile().toURI().toString();
+			MediaPlayer Player = new MediaPlayer(new Media(uriString));
+			Player.play();
+		} catch (Exception ex) {
+			log.error("Error while playing media" + ex.getMessage(), ex);
+		}
+	}
 
 }
